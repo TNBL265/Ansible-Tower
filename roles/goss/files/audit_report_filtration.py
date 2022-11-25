138d6661993177a20b2958e06336c44949914d9e
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-11-25 11:50:00 +0800

    test other OSes

--- roles/goss/files/audit_report_filtration.py
+++ roles/goss/files/audit_report_filtration.py
@@ -4,7 +4,7 @@ import sys
 
 
 class AuditReportFilter:
-    def __init__(self, os_report, raw_report, audit_data_filename, lookup_data_filename):
+    def __init__(self, os_report, raw_report, lookup_data_filename):
         self.os_report = os_report
         self.raw_report = raw_report
 
@@ -12,7 +12,7 @@ class AuditReportFilter:
         self.os_type = self.enum(UBUNTU18=0, UBUNTU20=1, UBUNTU22=2,
                                  AMZLNX2=3, RHEL7=4, RHEL8=5, RHEL9=6)
 
-        self.load_preconditions(audit_data_filename, lookup_data_filename)
+        self.load_preconditions(lookup_data_filename)
 
     def enum(self, **named_values):
         """
@@ -20,29 +20,10 @@ class AuditReportFilter:
         """
         return type("Enum", (), named_values)
 
-    def load_preconditions(self, audit_data_filename, lookup_data_filename):
+    def load_preconditions(self, lookup_data_filename):
         """
         Load certain pre conditions for each os-audit report, includes the CIS-manual audit
         """
-        with open(f"{audit_data_filename}", "r+") as audit_data:
-            audit_conditions = json.load(audit_data)
-        audit_data.close()
-
-        if self.os_report == self.os_type.UBUNTU18:
-            self.preconditions = audit_conditions["ubuntu18"]
-        elif self.os_report == self.os_type.UBUNTU20:
-            self.preconditions = audit_conditions["ubuntu20"]
-        elif self.os_report == self.os_type.UBUNTU22:
-            self.preconditions = audit_conditions["ubuntu22"]
-        elif self.os_report == self.os_type.AMZLNX2:
-            self.preconditions = audit_conditions["amazon_linux2"]
-        elif self.os_report == self.os_type.RHEL7:
-            self.preconditions = audit_conditions["rhel7"]
-        elif self.os_report == self.os_type.RHEL8:
-            self.preconditions = audit_conditions["rhel8"]
-        elif self.os_report == self.os_type.RHEL9:
-            self.preconditions = audit_conditions["rhel9"]
-
         # Load lookup table for determining pass fail for each CIS rule
         with open(f"{lookup_data_filename}", "r+") as lookup_data:
             self.lookup_table = json.load(lookup_data)
@@ -58,9 +39,6 @@ class AuditReportFilter:
         By default extract only property, success, title from the raw report, upon request can 
         extract more info if found from the raw report
         """
-        # lockdown_manual_list = self.preconditions["lockdown_manual_list"]
-        # cis_manual_list = self.preconditions["cis_manual_list"]
-        # pass_condition_dict = self.preconditions["special_pass_conditions"]
         cis_manual_list = []
         new_report = {}
 
@@ -69,7 +47,6 @@ class AuditReportFilter:
             if title != 'Benchmark MetaData':
                 if not title in list(new_report.keys()):
                     new_report[title] = {"result": {}}
-                cis_id = self.raw_report['results'][i]['meta']['CIS_ID']
                 result = self.raw_report['results'][i]['successful']
                 audit_type = self.raw_report['results'][i]['property']
 
@@ -78,46 +55,6 @@ class AuditReportFilter:
                     actual_id = title.split("|")[0].strip()
                     actual_manual_title = actual_id + " | " + actual_title
                     cis_manual_list.append(actual_manual_title)
-
-                #                if isinstance(cis_id, str):
-                #                    new_report[title]["cid_id"] = cis_id
-                #                    if cis_id in lockdown_manual_list:
-                #                        # If it is a manual test no need to report pass or fail, just 'manual' will do
-                #                        if "pass" in new_report[title]["result"] and "fail" in new_report[title]["result"]:
-                #                            new_report[title]["result"].pop("pass")
-                #                            new_report[title]["result"].pop("fail")
-                #                        new_report[title]["result"] = "manual"
-                #                elif isinstance(cis_id, list):
-                #                    if len(cis_id) == 1:
-                #                        new_report[title]["cid_id"] = str(cis_id[0])
-                #                        # if cis_id length only 1 we can proceed to pop the result
-                #                        if str(cis_id[0]) in lockdown_manual_list:
-                #                            if "pass" in new_report[title]["result"] and "fail" in new_report[title]["result"]:
-                #                                new_report[title]["result"].pop("pass")
-                #                                new_report[title]["result"].pop("fail")
-                #                            new_report[title]["result"] = "manual"
-                #                    else:
-                #                        # cis_id list that dont have manual test
-                #                        new_report[title]["cid_id"] = []
-                #                        for id in cis_id:
-                #                            new_report[title]["cid_id"].append(str(id))
-                #
-                #                else:
-                #                    # for cis_id that is integer
-                #                    new_report[title]["cid_id"] = str(cis_id)
-                #                    if str(cis_id) in lockdown_manual_list:
-                #                        if "pass" in new_report[title]["result"] and "fail" in new_report[title]["result"]:
-                #                            new_report[title]["result"].pop("pass")
-                #                            new_report[title]["result"].pop("fail")
-                #                        new_report[title]["result"] = "manual"
-
-                #                # if the result is str it is a manual audit, can be ignored
-                #                if not isinstance(new_report[title]['result'], str):
-                #                    if not result:
-                #                        new_report[title]["result"]["fail"] = new_report[title]["result"]["fail"] + 1
-                #                    elif result:
-                #                        new_report[title]["result"]["pass"] = new_report[title]["result"]["pass"] + 1
-                # add audit_type to result directly
                 new_report[title]["result"][audit_type] = str(result)
 
         # Pass fail criteria for each audit based on audit type, if found a fail in each audit type, result is fail
@@ -183,16 +120,14 @@ class AuditReportFilter:
 
 if __name__ == "__main__":
     #################################
-    # Only for temporay usage       #
+    # Only for temporary usage      #
     #################################
-    if len(sys.argv) == 5:
-        audit_data_filename = sys.argv[1]
-        lookup_data_filename = sys.argv[2]
-        audit_os_type = sys.argv[3]
-        # refined_report_name = sys.argv[2]
-        raw_report_name = sys.argv[4]
+    if len(sys.argv) == 4:
+        lookup_data_filename = sys.argv[1]
+        audit_os_type = sys.argv[2]
+        raw_report_name = sys.argv[3]
     else:
-        print("Usage:\npython3 {audit_data_filename} {lookup_data_filename} {audit_os_type} {raw_report_name}")
+        print("Usage:\npython3 {lookup_data_filename} {audit_os_type} {raw_report_name}")
         exit(1)
 
     support_os_type = ["ubuntu18", "ubuntu20", "ubuntu22", "amazon_linux2", "rhel7", "rhel8", "rhel9"]
@@ -217,22 +152,9 @@ if __name__ == "__main__":
             elif audit_os_type.lower() == "rhel9":
                 audit_os = 6
 
-    # raw_report_list = os.listdir(f"{os.path.abspath(os.curdir)}/raw_report")
-    # if raw_report_name not in raw_report_list:
-    #     print(
-    #         f"[Error] Unable to find the raw_report: {raw_report_name}, please make sure it is in json format and located in raw_report folder")
-    #     exit(1)
-    #################################
-    #    for file in os.listdir(f"{os.path.abspath(os.curdir)}/raw_report"):
-    #        if not (files.endswith('.json')) or not (files == raw_report_name):
-    #            print("[Error] Unable to find the raw_report: {raw_report_name}, please make sure it is json format and located in raw_report folder")
-    #            exit(1)
-
-    #    read_raw_report = open(f"{os.path.abspath(os.curdir)}/{f_name}",)
     read_raw_report = open(f"{raw_report_name}", )
     audit_raw_report = json.load(read_raw_report)
     read_raw_report.close()
-    report = AuditReportFilter(audit_os, audit_raw_report, audit_data_filename, lookup_data_filename)
+    report = AuditReportFilter(audit_os, audit_raw_report, lookup_data_filename)
     report.extract_data()
     print(report.refined_report)
-    # report.write_report(refined_report_name, f"{os.getcwd()}/report_refined")
